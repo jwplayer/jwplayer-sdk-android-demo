325b91599f4d5bddfca09684e492593943c631dd
gmsyrimis <gmsyrimis@gmail.com>
2019-10-28 15:00:43 -0400

    Updated: to stable version of SDK, improved logging in app, androidx libs
    Update

--- app/src/main/java/com/jwplayer/opensourcedemo/JWPlayerFragmentExample.java
+++ app/src/main/java/com/jwplayer/opensourcedemo/JWPlayerFragmentExample.java
@@ -2,101 +2,92 @@ package com.jwplayer.opensourcedemo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.longtailvideo.jwplayer.JWPlayerSupportFragment;
 import com.longtailvideo.jwplayer.JWPlayerView;
 import com.longtailvideo.jwplayer.configuration.PlayerConfig;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 public class JWPlayerFragmentExample extends AppCompatActivity {
 
-    /**
-     * A reference to the {@link JWPlayerSupportFragment}.
-     */
-    private JWPlayerSupportFragment mPlayerFragment;
 
-    /**
-     * A reference to the {@link JWPlayerView} used by the JWPlayerSupportFragment.
-     */
-    private JWPlayerView mPlayerView;
+	private JWPlayerSupportFragment mPlayerFragment;
 
-    /**
-     * An instance of our event handling class
-     */
-    private JWEventHandler mEventHandler;
+	private JWPlayerView mPlayerView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_jwplayerfragment);
+	private CallbackScreen mCallbackScreen;
 
-        TextView outputTextView = (TextView) findViewById(R.id.output);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_jwplayerfragment);
 
-        // Construct a new JWPlayerSupportFragment (since we're using AppCompatActivity)
-        mPlayerFragment = JWPlayerSupportFragment.newInstance(new PlayerConfig.Builder()
-                .file("http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8")
-                .build());
+		// Construct a new JWPlayerSupportFragment (since we're using AppCompatActivity)
+		mPlayerFragment = JWPlayerSupportFragment.newInstance(new PlayerConfig.Builder()
+																	  .file("http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8")
+																	  .build());
 
-        // Attach the Fragment to our layout
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.add(R.id.fragment_container, mPlayerFragment);
-        ft.commit();
+		// Attach the Fragment to our layout
+		FragmentManager fm = getSupportFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		ft.add(R.id.fragment_container, mPlayerFragment);
+		ft.commit();
 
-        // Make sure all the pending fragment transactions have been completed, otherwise
-        // mPlayerFragment.getPlayer() may return null
-        fm.executePendingTransactions();
+		// Make sure all the pending fragment transactions have been completed, otherwise
+		// mPlayerFragment.getPlayer() may return null
+		fm.executePendingTransactions();
 
-        // Get a reference to the JWPlayerView from the fragment
-        mPlayerView = mPlayerFragment.getPlayer();
+		// Get a reference to the JWPlayerView from the fragment
+		mPlayerView = mPlayerFragment.getPlayer();
 
-        // Keep the screen on during playback
-        new KeepScreenOnHandler(mPlayerView, getWindow());
+		// Keep the screen on during playback
+		new KeepScreenOnHandler(mPlayerView, getWindow());
 
-        // Instantiate the JW Player event handler class
-        mEventHandler = new JWEventHandler(mPlayerView, outputTextView);
-    }
+		// Event Logging
+		mCallbackScreen = findViewById(R.id.callback_screen);
+		mCallbackScreen.registerListeners(mPlayerView);
+	}
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        // Exit fullscreen when the user pressed the Back button
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            if (mPlayerView.getFullscreen()) {
-                mPlayerView.setFullscreen(false, true);
-                return false;
-            }
-        }
-        return super.onKeyDown(keyCode, event);
-    }
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// Exit fullscreen when the user pressed the Back button
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			if (mPlayerView.getFullscreen()) {
+				mPlayerView.setFullscreen(false, true);
+				return false;
+			}
+		}
+		return super.onKeyDown(keyCode, event);
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_jwplayerfragment, menu);
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_jwplayerfragment, menu);
 
-        // Register the MediaRouterButton
-        CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu,
-                R.id.media_route_menu_item);
+		// Register the MediaRouterButton
+		CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu,
+												R.id.media_route_menu_item);
 
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.switch_to_view:
-                Intent i = new Intent(this, JWPlayerViewExample.class);
-                startActivity(i);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.switch_to_view:
+				Intent i = new Intent(this, JWPlayerViewExample.class);
+				startActivity(i);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
 }

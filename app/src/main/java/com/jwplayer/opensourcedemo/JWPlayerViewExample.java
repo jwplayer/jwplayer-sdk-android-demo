b41727a3edad41cde357ac7f7b024dce6d012d91
George Syrimis <gmsyrimis@gmail.com>
2019-11-04 15:43:12 -0500

    [SDK-4841] Updated to latest stable SDK (#91)
    * Updated: to stable version of SDK, improved logging in app, androidx libs
    
    Update
    
    * reverted back to support libraries

--- app/src/main/java/com/jwplayer/opensourcedemo/JWPlayerViewExample.java
+++ app/src/main/java/com/jwplayer/opensourcedemo/JWPlayerViewExample.java
@@ -3,13 +3,11 @@ package com.jwplayer.opensourcedemo;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.CastContext;
@@ -19,150 +17,133 @@ import com.longtailvideo.jwplayer.events.listeners.VideoPlayerEvents;
 import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
 
 
+
 public class JWPlayerViewExample extends AppCompatActivity
-        implements VideoPlayerEvents.OnFullscreenListener {
+		implements VideoPlayerEvents.OnFullscreenListener {
 
-    /**
-     * Reference to the {@link JWPlayerView}
-     */
-    private JWPlayerView mPlayerView;
+	private JWPlayerView mPlayerView;
 
-    /**
-     * An instance of our event handling class
-     */
-    private JWEventHandler mEventHandler;
+	private CastContext mCastContext;
 
-    /**
-     * Reference to the {@link CastContext}
-     */
-    private CastContext mCastContext;
+	private CallbackScreen mCallbackScreen;
 
-    /**
-     * Stored instance of CoordinatorLayout
-     * http://developer.android.com/reference/android/support/design/widget/CoordinatorLayout.html
-     */
-    private CoordinatorLayout mCoordinatorLayout;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_jwplayerview);
+
+		mPlayerView = findViewById(R.id.jwplayer);
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_jwplayerview);
+		// Handle hiding/showing of ActionBar
+		mPlayerView.addOnFullscreenListener(this);
 
-        mPlayerView = findViewById(R.id.jwplayer);
-        TextView outputTextView = findViewById(R.id.output);
+		// Keep the screen on during playback
+		new KeepScreenOnHandler(mPlayerView, getWindow());
 
-        mCoordinatorLayout = findViewById(R.id.activity_jwplayerview);
+		// Event Logging
+		mCallbackScreen = findViewById(R.id.callback_screen);
+		mCallbackScreen.registerListeners(mPlayerView);
 
-        // Handle hiding/showing of ActionBar
-        mPlayerView.addOnFullscreenListener(this);
+		// Load a media source
+		PlaylistItem pi = new PlaylistItem.Builder()
+				.file("https://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8")
+				.title("BipBop")
+				.description("A video player testing video.")
+				.build();
 
-        // Keep the screen on during playback
-        new KeepScreenOnHandler(mPlayerView, getWindow());
+		mPlayerView.load(pi);
 
-        // Instantiate the JW Player event handler class
-        mEventHandler = new JWEventHandler(mPlayerView, outputTextView);
-
-        // Load a media source
-        PlaylistItem pi = new PlaylistItem.Builder()
-                .file("http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8")
-                .title("BipBop")
-                .description("A video player testing video.")
-                .build();
-
-        mPlayerView.load(pi);
-
-        // Get a reference to the CastContext
-        mCastContext = CastContext.getSharedInstance(this);
-    }
+		// Get a reference to the CastContext
+		mCastContext = CastContext.getSharedInstance(this);
 
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mPlayerView.onStart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mPlayerView.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mPlayerView.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mPlayerView.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mPlayerView.onDestroy();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        // Set fullscreen when the device is rotated to landscape
-        mPlayerView.setFullscreen(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE,
-                true);
-        super.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        // Exit fullscreen when the user pressed the Back button
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            if (mPlayerView.getFullscreen()) {
-                mPlayerView.setFullscreen(false, true);
-                return false;
-            }
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-
-    @Override
-    public void onFullscreen(FullscreenEvent fullscreenEvent) {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            if (fullscreenEvent.getFullscreen()) {
-                actionBar.hide();
-            } else {
-                actionBar.show();
-            }
-        }
-
-        // When going to Fullscreen we want to set fitsSystemWindows="false"
-        mCoordinatorLayout.setFitsSystemWindows(!fullscreenEvent.getFullscreen());
-    }
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_jwplayerview, menu);
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mPlayerView.onStart();
+	}
 
-        // Register the MediaRouterButton
-        CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu,
-                R.id.media_route_menu_item);
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mPlayerView.onResume();
+	}
 
-        return true;
-    }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mPlayerView.onPause();
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.switch_to_fragment:
-                Intent i = new Intent(this, JWPlayerFragmentExample.class);
-                startActivity(i);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+	@Override
+	protected void onStop() {
+		super.onStop();
+		mPlayerView.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mPlayerView.onDestroy();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		// Set fullscreen when the device is rotated to landscape
+		mPlayerView.setFullscreen(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE,
+								  true);
+		super.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// Exit fullscreen when the user pressed the Back button
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			if (mPlayerView.getFullscreen()) {
+				mPlayerView.setFullscreen(false, true);
+				return false;
+			}
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public void onFullscreen(FullscreenEvent fullscreenEvent) {
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			if (fullscreenEvent.getFullscreen()) {
+				actionBar.hide();
+			} else {
+				actionBar.show();
+			}
+		}
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_jwplayerview, menu);
+
+		// Register the MediaRouterButton
+		CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu,
+												R.id.media_route_menu_item);
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.switch_to_fragment:
+				Intent i = new Intent(this, JWPlayerFragmentExample.class);
+				startActivity(i);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
 }
